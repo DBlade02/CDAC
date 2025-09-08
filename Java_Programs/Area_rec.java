b01a7334668229b88ff08d3ee97fb7595b979183
Aditya Mogre <111582029+DBlade02@users.noreply.github.com>
2025-09-08 21:34:17 +0530

    new

---
+++ Java_Programs/Area_rec.java
@@ -0,0 +1,15 @@
+package Java_Programs;
+
+public class Area_rec {
+    public static void main(String[] args) {
+        
+        double length = 10.5; // Example length
+        double width = 5.0;  // Example width
+
+        double area = length * width; // Calculate the area
+
+        System.out.println("The length of the rectangle is: " + length);
+        System.out.println("The width of the rectangle is: " + width);
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
