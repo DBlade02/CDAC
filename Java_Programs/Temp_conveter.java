62ffd94de313f9868643572d1b07d269b57033d0
Aditya Mogre <111582029+DBlade02@users.noreply.github.com>
2025-09-09 22:25:06 +0530

    java

--- Java_Programs/Temp_conveter.java
+++ Java_Programs/Temp_conveter.java
@@ -3,11 +3,11 @@ package Java_Programs;
 import java.util.Scanner;
 public class Temp_conveter {
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
+        Scanner sc = new Scanner(System.in);
             System.out.print("Enter the Celsius: ");
             double celsius = sc.nextDouble(); // input Celsius temperature
             double fahrenheit = (celsius * 9/5) + 32; // Converting into fahrenheit
             System.out.println(celsius + " degrees Celsius is equal to " + fahrenheit + " degrees Fahrenheit.");
-        }
+        
     }
 }
---
+++ Java_Programs/Volume_Spear.java
@@ -0,0 +1,14 @@
+//Determine the volume of a sphere given its radius using the formula: V = (4/3) * π * r^3.
+package Java_Programs;
+import java.util.Scanner;
+public class Volume_Spear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the radius of the sphere: ");
+        double radius = sc.nextDouble();
+        final double PI = 3.14159;
+        double volume = (4.0/3.0) * PI * Math.pow(radius,3);
+        System.out.println("The volume of the sphere with radius " + radius + " is: " + volume);
+        sc.close();
+    }
+}
