96c8c3dce0820976b6800b0721594568e4b174ce
Aditya Mogre <111582029+DBlade02@users.noreply.github.com>
2025-09-09 22:30:33 +0530

    java

--- Java_Programs/NaturalNo_Sum.java
+++ Java_Programs/NaturalNo_Sum.java
@@ -5,18 +5,16 @@ public class NaturalNo_Sum
 {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a positive integer: ");// Take input from the user
-        long n = sc.nextLong();
-        if(n<0)
-        {
-            System.out.println("Please enter a positive integer.");// Handle negative input
-        }
-        else
-        {
-            long sum = n * (n + 1) / 2; // Using the formula to calculate the sum
-            System.out.println("The sum of the first " + n + " natural numbers is: " + sum);
-        }
-        sc.close(); // Close the scanner to prevent resource leaks
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a positive integer: ");// Take input from the user
+            long n = sc.nextLong();
+            if (n<0) {
+                System.out.println("Please enter a positive integer.");// Handle negative input
+            } else {
+                long sum = n * (n + 1) / 2; // Using the formula to calculate the sum
+                System.out.println("The sum of the first " + n + " natural numbers is: " + sum);
+            }
+            // Close the scanner to prevent resource leaks
+        } // Take input from the user
     }  
 }
