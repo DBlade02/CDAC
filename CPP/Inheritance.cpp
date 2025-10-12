c898807c6736f280be5f59f2e79213934a3c211e
Aditya Mogre <111582029+DBlade02@users.noreply.github.com>
2025-10-12 13:59:43 +0530

    CPP

---
+++ CPP/Inheritance.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class A{
+    public:
+    void display(){
+        cout<<"This is A class"<<endl;
+    }
+};
+
+class B : public A{
+    public:
+    void display(){
+        A::display();
+        cout<< "This is B class "<< endl;
+        
+    }
+        
+};
+
+int main(){
+    B obj;
+    obj.display();
+}
