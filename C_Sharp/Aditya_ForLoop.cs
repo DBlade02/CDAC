25f1b613ce5c9b9584c606bcf91423de0621702b
Aditya Mogre <111582029+DBlade02@users.noreply.github.com>
2025-11-08 15:38:37 +0530

    Add Aditya_ForLoop example in C#

---
+++ C_Sharp/Aditya_ForLoop.cs
@@ -0,0 +1,17 @@
+using System;
+
+
+namespace AdityaExamples
+{
+internal class Aditya_ForLoop
+{
+static void Main()
+{
+Console.WriteLine("Counting for Aditya:");
+for (int i = 1; i <= 5; i++)
+{
+Console.WriteLine(i);
+}
+}
+}
+}
