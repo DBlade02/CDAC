f40be2695dddc6cb1462dabc2d559d2873e4e1b1
Aditya Mogre <111582029+DBlade02@users.noreply.github.com>
2025-11-08 15:39:02 +0530

    Add Aditya_WhileLoop.cs with a while loop example

---
+++ C_Sharp/Aditya_WhileLoop.cs
@@ -0,0 +1,19 @@
+using System;
+
+
+namespace AdityaExamples
+{
+internal class Aditya_WhileLoop
+{
+static void Main()
+{
+int i = 1;
+Console.WriteLine("While loop for Aditya:");
+while (i <= 5)
+{
+Console.WriteLine(i);
+i++;
+}
+}
+}
+}
