ff0854850cc8b0c454671b4c9df01a3364fab520
Aditya Mogre <111582029+DBlade02@users.noreply.github.com>
2025-11-08 15:37:07 +0530

    Add Aditya_IfElse.cs for student pass/fail check

---
+++ C_Sharp/Aditya_IfElse.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+namespace AdityaExamples
+{
+internal class Aditya_IfElse
+{
+static void Main()
+{
+int mark = 75;
+Console.WriteLine("Student: Aditya");
+if (mark >= 60)
+Console.WriteLine("Result: Pass");
+else
+Console.WriteLine("Result: Fail");
+}
+}
+}
