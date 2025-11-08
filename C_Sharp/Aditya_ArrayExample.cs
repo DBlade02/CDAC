4725b2e2bf4802974244f16e7244ee29d71e1355
Aditya Mogre <111582029+DBlade02@users.noreply.github.com>
2025-11-08 15:39:36 +0530

    Add Aditya_ArrayExample.cs for displaying friends list

---
+++ C_Sharp/Aditya_ArrayExample.cs
@@ -0,0 +1,16 @@
+using System;
+
+
+namespace AdityaExamples
+{
+internal class Aditya_ArrayExample
+{
+static void Main()
+{
+string[] friends = { "Amit", "Sunil", "Aditya", "Meena" };
+Console.WriteLine("Friends list (including Aditya):");
+foreach (var f in friends)
+Console.WriteLine(f);
+}
+}
+}
