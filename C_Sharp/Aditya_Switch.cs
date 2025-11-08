74dcf48817f27debd25d756131951a2cd076557c
Aditya Mogre <111582029+DBlade02@users.noreply.github.com>
2025-11-08 15:37:55 +0530

    Add Aditya_Switch class with switch statement example

---
+++ C_Sharp/Aditya_Switch.cs
@@ -0,0 +1,21 @@
+using System;
+
+
+namespace AdityaExamples
+{
+internal class Aditya_Switch
+{
+static void Main()
+{
+int day = 3;
+Console.Write("Aditya's day number: ");
+switch (day)
+{
+case 1: Console.WriteLine("Monday"); break;
+case 2: Console.WriteLine("Tuesday"); break;
+case 3: Console.WriteLine("Wednesday"); break;
+default: Console.WriteLine("Other day"); break;
+}
+}
+}
+}
