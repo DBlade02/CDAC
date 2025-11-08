65463fbaa3fcb9091bea892ece632bd0f55b32a9
Aditya Mogre <111582029+DBlade02@users.noreply.github.com>
2025-11-08 15:40:36 +0530

    Create Aditya_MethodExample.cs

---
+++ C_Sharp/Aditya_MethodExample.cs
@@ -0,0 +1,19 @@
+using System;
+
+
+namespace AdityaExamples
+{
+internal class Aditya_MethodExample
+{
+static void Main()
+{
+Greet("Aditya");
+}
+
+
+static void Greet(string name)
+{
+Console.WriteLine($"Hello {name}, welcome to C# methods!");
+}
+}
+}
