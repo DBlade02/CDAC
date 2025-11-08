9bcb191a84e4988b0b60f6d2cb1ae9c7f9290564
Aditya Mogre <111582029+DBlade02@users.noreply.github.com>
2025-11-08 15:36:40 +0530

    Add Aditya_Variables class with main method

---
+++ C_Sharp/Aditya_Variables.cs
@@ -0,0 +1,16 @@
+using System;
+
+
+namespace AdityaExamples
+{
+internal class Aditya_Variables
+{
+static void Main()
+{
+string name = "Aditya";
+int year = 2025;
+double cgpa = 7.02;
+Console.WriteLine($"Name: {name}, Year: {year}, CGPA: {cgpa}");
+}
+}
+}
